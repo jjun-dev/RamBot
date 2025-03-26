750cd0f35dfe3df9f55010dd1392bf89ef137532
jjun <jjun010108@naver.com>
2025-03-26 14:33:17 +0900

    edit

---
+++ RamBotPackage/src/voice_commands.cpp
@@ -0,0 +1,9 @@
+#include "voice_commands.h"
+
+
+void voiceCommands:: handleCommands(const dpp::slashcommand_t& event, const std::string& command) {
+
+}
+void voicdCommands::joinVoiceChannel(const dpp::slashcommand_t& event);
+
+
